4d94cbb5f940b532f03d2cafe1135b4e212dcdab
Bing <bing334@gmail.com>
2026-05-25 19:35:23 -0400

    EJS Tool for layouts
    - install and setup ejs-mate
    - setup base boilerplate
    - using bolierplate in all museum page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Museum = require('./models/museum');
 
@@ -17,7 +18,7 @@ db.once("open", () => {
     console.log("Database connected")
 });
 
-
+app.engine('ejs',ejsMate)
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname,'views'))
 // Middleware that parses incoming HTTP requests with form data
