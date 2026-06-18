67112c2907439f2acf80a5f72eeb4d7e59f74dfb
Bing <bing334@gmail.com>
2026-06-17 20:40:20 -0400

    defining ExpressError class
    - created catchAsync and expressError class
    - applied catchAsync to routes
    - add basic error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Museum = require('./models/museum');
 
@@ -33,44 +34,47 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 // GET Route: all museums
-app.get('/museums', async (req, res) => {
+app.get('/museums', catchAsync(async (req, res) => {
     const museums = await Museum.find({});
     res.render('museums/index',{museums})
-})
+}))
 // GET Route: new museum form
 app.get('/museums/new', (req, res) => {
     res.render('museums/new')
 })
 // POST Route: post new museum info, then direct to that musem page
-app.post('/museums', async (req, res) => {
+app.post('/museums', catchAsync(async (req, res, next) => {
     const museum = new Museum(req.body.museum);
     await museum.save();
     res.redirect(`/museums/${museum._id}`)
-})
+}))
 // GET Route: get museum detail by id
-app.get('/museums/:id', async (req, res) => {
+app.get('/museums/:id', catchAsync(async (req, res) => {
     const museum = await Museum.findById(req.params.id)
     res.render('museums/show',{museum})
-})
+}))
 // GET Route: edit museum info form
-app.get('/museums/:id/edit', async (req, res) => {
+app.get('/museums/:id/edit', catchAsync(async (req, res) => {
     const museum = await Museum.findById(req.params.id)
     res.render('museums/edit',{museum})
-})
+}))
 // PUT Route: update the museum
-app.put('/museums/:id', async (req, res) => {
+app.put('/museums/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const museum = await Museum.findByIdAndUpdate(id, {...req.body.museum})
     res.redirect(`/museums/${museum._id}`)
-})
+}))
 // DELETE Route: delete the museum
-app.delete('/museums/:id', async (req, res) => {
+app.delete('/museums/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Museum.findByIdAndDelete(id);
     res.redirect(`/museums`);
+}))
+
+// Basic Error Handler
+app.use((err,req,res) => {
+    res.send('Error, Something went wrong !')
 })
-
-
 
 app.listen(3000, () => {
     console.log('Serviing on port 3000')
